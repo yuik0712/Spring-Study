5c17f74bf389af0f52b7531bf07f6077064ec75d
yuik <163374559+yuik0712@users.noreply.github.com>
2025-02-17 18:21:08 +0900

    깃 커밋이 누락되어 재 커밋

---
+++ also/veneer/Spring-MVC/src/main/java/com/example/Spring_MVC/SpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Spring Boot 애플리케이션의 진입점 (Main 클래스)
+@SpringBootApplication
+public class SpringMvcApplication {
+	public static void main(String[] args) {
+		// Spring boot 애플리케이션 실행
+		SpringApplication.run(SpringMvcApplication.class, args);
+	}
+}
