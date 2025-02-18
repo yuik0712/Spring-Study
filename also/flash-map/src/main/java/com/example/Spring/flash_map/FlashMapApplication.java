4fe1b59a1efaef995f9511db5eec3a51471a6864
yuik <163374559+yuik0712@users.noreply.github.com>
2025-02-19 00:24:07 +0900

    [feat] Flash-Map 스프링 프로젝트 예제 커밋

---
+++ also/flash-map/src/main/java/com/example/Spring/flash_map/FlashMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.flash_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashMapApplication.class, args);
+	}
+
+}
