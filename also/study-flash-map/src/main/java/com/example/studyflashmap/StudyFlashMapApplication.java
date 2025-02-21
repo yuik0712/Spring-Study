768fb3082b2bf5ec80ba42a9f316d8ba2cfbb26d
yuik <163374559+yuik0712@users.noreply.github.com>
2025-02-21 19:45:22 +0900

    첫 커밋

---
+++ also/study-flash-map/src/main/java/com/example/studyflashmap/StudyFlashMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyflashmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFlashMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyFlashMapApplication.class, args);
+	}
+
+}
